96640db72fdb974208242f8b57d42be99ef862de
Pekka Laiho <pekka.i.laiho@gmail.com>
2026-04-22 17:28:00 +0300

    Pico CSS

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        GolfTracker:
-        <?= $this->fetch('title') ?>
-    </title>
+    <title>GolfTracker: <?= $this->fetch('title') ?></title>
     <?= $this->Html->meta('icon') ?>
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
+    <?= $this->Html->css(['pico.classless.orange.css']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>">Golf<span>Tracker</span></a>
-        </div>
-        <div class="top-nav-links">
-            <?= $this->Html->link('Rounds', ['controller' => 'Rounds', 'action' => 'index']) ?>
-            <?= $this->Html->link('Courses', ['controller' => 'Courses', 'action' => 'index']) ?>
-            <?= $this->Html->link('Players', ['controller' => 'Players', 'action' => 'index']) ?>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
-            <?= $this->fetch('content') ?>
-        </div>
+    <header>
+        <nav>
+            <ul>
+                <li>GolfTracker</li>
+            </ul>
+            <ul>
+                <li><?= $this->Html->link('Rounds', ['controller' => 'Rounds', 'action' => 'index']) ?></li>
+                <li><?= $this->Html->link('Courses', ['controller' => 'Courses', 'action' => 'index']) ?></li>
+                <li><?= $this->Html->link('Players', ['controller' => 'Players', 'action' => 'index']) ?></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <?= $this->Flash->render() ?>
+        <?= $this->fetch('content') ?>
     </main>
     <footer>
     </footer>
--- templates/layout/error.php
+++ templates/layout/error.php
@@ -1,39 +1,36 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- * @var \App\View\AppView $this
- */
-?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <?= $this->Html->charset() ?>
-    <title>
-        <?= $this->fetch('title') ?>
-    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>GolfTracker: <?= $this->fetch('title') ?></title>
     <?= $this->Html->meta('icon') ?>
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'fonts', 'cake']) ?>
+    <?= $this->Html->css(['pico.classless.orange.css']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <div class="error-container">
+    <header>
+        <nav>
+            <ul>
+                <li>GolfTracker</li>
+            </ul>
+            <ul>
+                <li><?= $this->Html->link('Rounds', ['controller' => 'Rounds', 'action' => 'index']) ?></li>
+                <li><?= $this->Html->link('Courses', ['controller' => 'Courses', 'action' => 'index']) ?></li>
+                <li><?= $this->Html->link('Players', ['controller' => 'Players', 'action' => 'index']) ?></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
         <?= $this->Flash->render() ?>
         <?= $this->fetch('content') ?>
         <?= $this->Html->link(__('Back'), 'javascript:history.back()') ?>
-    </div>
+    </main>
+    <footer>
+    </footer>
 </body>
 </html>
